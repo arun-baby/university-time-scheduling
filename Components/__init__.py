d5261ae814a61d89da8fd95349fafb30c2340d2a
Arun Baby <arunbaby@iisc.ac.in>
2020-12-24 00:18:28 +0530

    Comments and bugfixes

--- Components/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Class import Class
-from .Course import Course
-from .Subpart import Subpart
-from .Timing import Timing
-from .SoftConstraint import SoftConstraint
-from .HardConstraint import HardConstraint
-from .Student import Student
