9c47acabe9c68a1860f01866d74f54348482fed5
Arun Baby <arunbaby95@gmail.com>
2021-02-15 15:59:16 +0530

    addition of age

--- Components/HardConstraint.py
+++ Components/HardConstraint.py
@@ -2,4 +2,5 @@ class HardConstraint:
     def __init__(self, type, classes):
         self.type = type
         self.classes = classes
-        
+        self.iterations = 0
+        self.timeouts = 0
